89e7830bae510951dc9f54448441b6c7c53cfbad
LUIS JOEL MENDEZ LOOR <e1312386921@live.uleam.edu.ec>
2019-10-21 13:03:43 -0500

    defi

---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ejercicio2.clases;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Nota n = new Nota();
+            Console.WriteLine("Ingrese su nombre:");
+            n.Nombre = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Ingrese nota de actuacion del primer parcial");
+                n.ActuacionP1 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese nota de producción del primer parcial");
+                n.ProduccionP1 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese nota de trabajo autonomo del primer parcial");
+                n.TrabajoAutonomoP1 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese nota de exámen del primer parcial");
+                n.ExamenFinalP1 = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese nota de actuacion del segundo parcial");
+                n.ActuacionP2 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese nota de producción del segundo parcial");
+                n.ProduccionP2 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese nota de trabajo autonomo del segundo parcial");
+                n.TrabajoAutonomoP2 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese nota de exámen del segundo parcial");
+                n.ExamenFinalP2 = float.Parse(Console.ReadLine());
+
+
+                if (n.PrimerParcial + n.PSegundoParcial < 14)
+                {
+                    if (n.PrimerParcial + n.PSegundoParcial >= 8)
+                    {
+
+                        Console.WriteLine("Usted esta en recuperación\n");
+                        Console.WriteLine("Ingrese nota de recuperación");
+                        n.NotaRecuperacion = float.Parse(Console.ReadLine());
+
+                        if (n.NotaRecuperacion >= n.Recuperacion)
+                        {
+                            Console.WriteLine("APROBADO!");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("REPROBADO");
+                        }
+
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("REPROBADO");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("APROBADO!");
+                }
+            }
+            catch (Exception mensaje)
+            {
+                Console.WriteLine(mensaje.Message);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ejercicio2/ejercicio2/clases/Nota.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2.clases
+{
+    
+        public class Nota
+        {
+            private string nombre;
+
+
+            public string Nombre
+            {
+                get { return nombre; }
+                set { nombre = value; }
+            }
+
+
+            //actividad en clases 25%
+            private float actuacionp1;
+
+            public float ActuacionP1
+            {
+                get { return actuacionp1; }
+                set
+                {
+
+                    if (value > 10 || value < 0)
+                        throw new Exception("La nota debe estar entre 0 y 10");
+
+                    actuacionp1 = value;
+
+
+                }
+            }
+
+
+            //Produccion 25%
+            private float produccionp1;
+
+            public float ProduccionP1
+            {
+
+                get { return produccionp1; }
+                set
+                {
+                    if (value > 10 || value < 0)
+                        throw new Exception("La nota debe estar entre 0 y 10");
+                    produccionp1 = value;
+                }
+            }
+
+
+            //traajo autonomo 20%
+
+            private float trabajoautonomop1;
+
+            public float TrabajoAutonomoP1
+            {
+                get { return trabajoautonomop1; }
+                set
+                {
+                    if (value > 10 || value < 0)
+                        throw new Exception("La nota debe estar entre 0 y 10");
+                    trabajoautonomop1 = value;
+                }
+            }
+
+            //examen 30%
+            private float examenfinalp1;
+
+            public float ExamenFinalP1
+            {
+                get { return examenfinalp1; }
+                set
+                {
+                    if (value > 10 || value < 0)
+                        throw new Exception("La nota debe estar entre 0 y 10");
+                    examenfinalp1 = value;
+                }
+            }
+
+            private float primerparcial;
+
+            public float PrimerParcial
+            {
+
+                get
+                {
+
+                    return ActuacionP1 * 0.25f + ProduccionP1 * 0.25f + TrabajoAutonomoP1 * 0.2f + ExamenFinalP1 * 0.3f;
+
+                }
+            }
+
+            private float actuacionp2;
+
+            public float ActuacionP2
+            {
+                get { return actuacionp1; }
+                set
+                {
+                    if (value > 10 || value < 0)
+                        throw new Exception("La nota debe estar entre 0 y 10");
+                    actuacionp2 = value;
+                }
+            }
+
+
+            //Produccion 25%
+            private float produccionp2;
+
+            public float ProduccionP2
+            {
+                get { return produccionp1; }
+                set
+                {
+                    if (value > 10 || value < 0)
+                        throw new Exception("La nota debe estar entre 0 y 10");
+                    produccionp2 = value;
+                }
+            }
+
+
+            //traajo autonomo 20%
+
+            private float trabajoautonomop2;
+
+            public float TrabajoAutonomoP2
+            {
+                get { return trabajoautonomop1; }
+                set
+                {
+                    if (value > 10 || value < 0)
+                        throw new Exception("La nota debe estar entre 0 y 10");
+                    trabajoautonomop2 = value;
+                }
+            }
+
+            //examen 30%
+            private float examenfinalp2;
+
+            public float ExamenFinalP2
+            {
+                get { return examenfinalp1; }
+                set
+                {
+                    if (value > 10 || value < 0)
+                        throw new Exception("La nota debe estar entre 0 y 10"); examenfinalp2 = value;
+                }
+            }
+
+            private float segundoparcial;
+
+            public float PSegundoParcial
+            {
+
+                get
+                {
+
+                    return ActuacionP2 * 0.25f + ProduccionP2 * 0.25f + TrabajoAutonomoP2 * 0.2f + ExamenFinalP2 * 0.3f;
+
+                }
+
+
+
+            }
+            private float recuperacion;
+
+            public float Recuperacion
+            {
+                get
+                {
+
+                    return 14 - (primerparcial + segundoparcial / 2);
+
+                }
+
+            }
+
+            private float notareacuperacion;
+
+            public float NotaRecuperacion
+            {
+                get { return notareacuperacion; }
+                set
+                {
+                    if (value > 10 || value < 0)
+                        throw new Exception("La nota debe estar entre 0 y 10");
+                    notareacuperacion = value;
+                }
+            }
+
+
+        }
+
+    }
+
